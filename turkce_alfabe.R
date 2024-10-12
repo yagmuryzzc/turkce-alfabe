4e41a1199a41502a1cba5eef572f3b029a6661f2
yagmuryzzc <116810567+yagmuryzzc@users.noreply.github.com>
2024-10-12 16:36:44 +0300

    Create turkce_alfabe.R
    türkçe alfabesini R paketi olarak yükler

---
+++ turkce_alfabe.R
@@ -0,0 +1,7 @@
+# Türk alfabesi
+turkce_alfabe <- c("A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", 
+                   "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", 
+                   "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z")
+
+# Sonucu yazdır
+print(turkce_alfabe)
